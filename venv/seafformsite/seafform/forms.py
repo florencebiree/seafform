62e0feb6e542f89a9d875de68ff355be37b1f720
Florian Birée <florian@biree.name>
2015-09-21 12:38:54 +0200

    add agpl licence

--- venv/seafformsite/seafform/forms.py
+++ venv/seafformsite/seafform/forms.py
@@ -7,16 +7,16 @@
 #       This file is a part of seafform.
 #       
 #       This program is free software: you can redistribute it and/or modify
-#       it under the terms of the GNU General Public License as published by
-#       the Free Software Foundation, either version 3 of the License, or
-#       (at your option) any later version.
+#       it under the terms of the GNU Affero General Public License as 
+#       published by the Free Software Foundation, either version 3 of the 
+#       License, or (at your option) any later version.
 #       
 #       This program is distributed in the hope that it will be useful,
 #       but WITHOUT ANY WARRANTY; without even the implied warranty of
 #       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#       GNU General Public License for more details.
+#       GNU Affero General Public License for more details.
 #       
-#       You should have received a copy of the GNU General Public License
+#       You should have received a copy of the GNU Affero General Public License
 #       along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #       
 ###############################################################################
@@ -24,10 +24,8 @@
 
 __author__ = "Florian Birée"
 __version__ = "0.1"
-__license__ = "GPLv3"
+__license__ = "AGPLv3"
 __copyright__ = "Copyright © 2015, Florian Birée <florian@biree.name>"
-__revision__ = "$Revision: $"
-__date__ = "$Date: $"
 
 from django import forms
 from django.forms.extras.widgets import SelectDateWidget
--- venv/seafformsite/seafformsite/urls.py
+++ venv/seafformsite/seafformsite/urls.py
@@ -1,11 +1,35 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#       seafformsite/urls.py
+#       
+#       Copyright © 2015, Florian Birée <florian@biree.name>
+#       
+#       This file is a part of seafform.
+#       
+#       This program is free software: you can redistribute it and/or modify
+#       it under the terms of the GNU Affero General Public License as 
+#       published by the Free Software Foundation, either version 3 of the 
+#       License, or (at your option) any later version.
+#       
+#       This program is distributed in the hope that it will be useful,
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#       GNU Affero General Public License for more details.
+#       
+#       You should have received a copy of the GNU Affero General Public License
+#       along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#       
+###############################################################################
+
+__author__ = "Florian Birée"
+__version__ = "0.1"
+__license__ = "AGPLv3"
+__copyright__ = "Copyright © 2015, Florian Birée <florian@biree.name>"
+
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'seafformsite.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('seafform.urls')),
 )
