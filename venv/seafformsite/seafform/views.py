a24a42bfda1b9467aee65244ea5515d6af7cc1d7
Florian Birée <florian@biree.name>
2017-09-17 19:01:53 +0200

    remove debuging code which failed with unicode file path

--- venv/seafformsite/seafform/views.py
+++ venv/seafformsite/seafform/views.py
@@ -179,7 +179,7 @@ def new(request):
     # if this is a POST request we need to process the form data
     if request.method == 'POST':
         path = unquote(request.POST.get('path', ''))
-        print('new/path=' + path)
+        #print('new/path=' + path)
         if path.endswith('.ods'):
             if settings.LOCAL:
                 filepath = os.path.join(settings.LOCAL_ROOT, path.lstrip('/'))
